e9db7fad4ccab7d5d775db51696f517a36020d19
Qualle <twiltwind44607@gmail.com>
2024-07-04 11:34:56 +0700

    feat<txt>: add text padding

--- lib/widgets/meow.dart
+++ lib/widgets/meow.dart
@@ -8,16 +8,55 @@ class Meow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
      return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/insanecat.gif'),
-            fit: BoxFit.cover,
+      body: Center(
+        child: Stack(children: <Widget>[
+          Positioned.fill(
+            child: ShaderMask(
+              shaderCallback: (rect) {
+                return LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.transparent,
+                    Colors.black.withOpacity(0.4),
+                  ],
+                ).createShader(rect);
+              },
+              blendMode: BlendMode.darken,
+              child: const Image(
+                image: AssetImage('assets/images/insanecat.gif'),
+                fit: BoxFit.cover,
           ),
         ),
-        child: null
-
-         /* add child content here */,
+      ),
+      const Positioned.fill(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Padding(
+              padding: EdgeInsets.fromLTRB(12, 0, 0, 0),
+              child: Text(
+              '[ Ulltimate Card ]',
+            style:  TextStyle(
+              color: Color.fromARGB(255, 255, 255, 255),
+              fontWeight: FontWeight.bold ,
+              fontSize: 30.0),
+            )    
+          ),
+          Padding(
+              padding: EdgeInsets.fromLTRB(12, 0, 12, 0),
+              child: Text(
+              'aaa aaa ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh!!!!!!!',
+            style:  TextStyle(
+              color: Color.fromARGB(255, 255, 255, 255),
+              fontSize: 15.0),
+                      ),
+                )
+              ],
+            ),
+          )
+        ]),
       ),
     );
   }
